8263441d10554a681a96083756e987a54aba67f9
MichaellaStar <Michaella.Starkie@codedbykids.org>
2026-09-22 20:36:37 +0000

    did intrustions

---
+++ app.js
@@ -0,0 +1,10 @@
+const name = "michaella " 
+let age = 15
+let isLearningJS = true
+
+console.log(name)
+console.log(age)
+console.log(isLearningJS)
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isLearningJS)
